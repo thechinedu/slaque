7cac7156d68ee339b128dfdc427da2e31ddbd36e
Chinedu Daniel <chinedudaniel7@gmail.com>
2022-04-07 08:54:12 +0200

    Start fleshing out auth workflow (#27)

--- pages/api/v1/users.ts
+++ pages/api/v1/users.ts
@@ -14,3 +14,32 @@ export default function handler(req: NextApiRequest, res: NextApiResponse) {
 
   executeMiddlewares(req, res);
 }
+
+/**
+ * User sign up and Authentication flow for base slaque
+ * ========================================================================
+ *
+ * When a user signs up for an account, a one time token is sent to the user's email address
+ * If the specified token is valid, the user is set as verified and the user is automatically
+ * logged in (email-based passwordless login)
+ *
+ * The one time token should expire in a few minutes (users can request a new token)
+ *
+ * if a user creates a new account but doesn't verify it, the account will be deleted after a few days
+ */
+
+/**
+ * User sign up and Authentication flow for a slaque workspace
+ * ========================================================================
+ *
+ * When a user signs up for an account via a workspace url, a confirmatiion email is sent to the user's email address
+ * on cofirmation of the email address, the user is set as verified
+ * A main user account is created and a workspace account is created (tied to the main user account)
+ * The user is required to enter their full name and a password for the workspace account
+ * Once the required fields are provided, the workspace user is automatically logged in
+ *
+ * if a user creates a new account but doesn't verify it (in this case, through the confirmation link)
+ * the account will be deleted after a few days
+ *
+ * Sign up through invitation will work similarly
+ */
