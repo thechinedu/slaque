452681df47dc4b94427d383e3ab42fa5794c9549
Chinedu Daniel <chinedudaniel7@gmail.com>
2022-05-17 09:25:16 +0200

    Iterate on first functional test

--- server/hooks/users.ts
+++ server/hooks/users.ts
@@ -1,6 +1,9 @@
 import { Middleware } from "@/types/shared";
 
 export const convertUserEmailToLowerCase: Middleware = (req, _res, next) => {
-  req.body.email = req.body.email.toLowerCase();
+  if (req.body?.email) {
+    req.body.email = req.body.email.toLowerCase();
+  }
+
   return next();
 };
